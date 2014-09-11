c5224272edd5973e66e21db64174e4a579f753f7
Tom Carchrae <tom@carchrae.net>
2014-09-10 23:03:46 -0700

    set endpoint based on test mode,  and other options

--- app/models/spree/calculator/shipping/canada_post/base.rb
+++ app/models/spree/calculator/shipping/canada_post/base.rb
@@ -6,24 +6,29 @@ module Spree
       class Base < Spree::Calculator::Shipping::ActiveShipping::Base
         def carrier
 
-          if (Spree::ActiveShipping::Config[:canada_post_login].present?)
+          if (!Spree::ActiveShipping::Config[:canada_post_login].blank?)
             puts "using CPCID"
             canada_post_options = {
                 :login => Spree::ActiveShipping::Config[:canada_post_login],
                 :french => (I18n.locale.to_sym == :fr)
             }
-            ActiveMerchant::Shipping::CanadaPost.new(canada_post_options)
+            carrier = ActiveMerchant::Shipping::CanadaPost.new(canada_post_options)
           else
-            if (Spree::ActiveShipping::Config[:canada_post_api_key].present?)
+            if (!Spree::ActiveShipping::Config[:canada_post_api_key].blank?)
               puts "using PWS"
               canada_post_options = {
                   :api_key => Spree::ActiveShipping::Config[:canada_post_api_key],
                   :secret => Spree::ActiveShipping::Config[:canada_post_secret],
-                  :french => (I18n.locale.to_sym == :fr)
+                  :endpoint => Spree::ActiveShipping::Config[:test_mode] ?
+                      'https://ct.soa-gw.canadapost.ca/' : nil,
+                  :customer_number => Spree::ActiveShipping::Config[:canada_post_customer_number],
+                  :language => I18n.locale.to_sym
               }
-              ActiveMerchant::Shipping::CanadaPostPWS.new(canada_post_options)
+              carrier = ActiveMerchant::Shipping::CanadaPostPWS.new(canada_post_options)
             end
           end
+
+          carrier
         end
       end
     end
