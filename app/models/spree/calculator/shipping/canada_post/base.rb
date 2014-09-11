ce792bfc7cbf602cdd6125f05e08d2ee354d529a
Tom Carchrae <tom@carchrae.net>
2014-09-10 21:45:01 -0700

    allow use of CanadaPost PWS

--- app/models/spree/calculator/shipping/canada_post/base.rb
+++ app/models/spree/calculator/shipping/canada_post/base.rb
@@ -5,11 +5,25 @@ module Spree
     module CanadaPost
       class Base < Spree::Calculator::Shipping::ActiveShipping::Base
         def carrier
-          canada_post_options = {
-            :login  => Spree::ActiveShipping::Config[:canada_post_login],
-            :french => (I18n.locale.to_sym == :fr)
-          }
-          ActiveMerchant::Shipping::CanadaPost.new(canada_post_options)
+
+          if (Spree::ActiveShipping::Config[:canada_post_login].present?)
+            puts "using CPCID"
+            canada_post_options = {
+                :login => Spree::ActiveShipping::Config[:canada_post_login],
+                :french => (I18n.locale.to_sym == :fr)
+            }
+            ActiveMerchant::Shipping::CanadaPost.new(canada_post_options)
+          else
+            if (Spree::ActiveShipping::Config[:canada_post_api_key].present?)
+              puts "using PWS"
+              canada_post_options = {
+                  :api_key => Spree::ActiveShipping::Config[:canada_post_api_key],
+                  :secret => Spree::ActiveShipping::Config[:canada_post_secret],
+                  :french => (I18n.locale.to_sym == :fr)
+              }
+              ActiveMerchant::Shipping::CanadaPostPWS.new(canada_post_options)
+            end
+          end
         end
       end
     end
